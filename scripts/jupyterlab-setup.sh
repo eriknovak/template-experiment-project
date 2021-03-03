38896f6bd755c1a9837033a71aabaef0f1f18b4c
Erik Novak <novak.erik@gmail.com>
2021-03-03 22:05:17 +0100

    Create jupyterlab-setup.sh

---
+++ scripts/jupyterlab-setup.sh
@@ -0,0 +1,5 @@
+# install the jupyterlab extensions
+jupyter labextension install @jupyter-widgets/jupyterlab-manager
+jupyter labextension install @jupyterlab/debugger
+jupyter labextension install @jupyterlab/toc
+jupyter labextension install plotlywidget
