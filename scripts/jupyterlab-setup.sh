e0c4e1b3041bd1f940c19055a0ac62b3f7b3a77d
Erik Novak <novak.erik@gmail.com>
2021-03-04 08:46:04 +0100

    Delete scripts directory

--- scripts/jupyterlab-setup.sh
+++
@@ -1,5 +0,0 @@
-# install the jupyterlab extensions
-jupyter labextension install @jupyter-widgets/jupyterlab-manager
-jupyter labextension install @jupyterlab/debugger
-jupyter labextension install @jupyterlab/toc
-jupyter labextension install plotlywidget
